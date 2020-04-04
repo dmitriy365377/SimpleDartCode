3d925bb0d5669f857769ddd52da0387ead3a8ee0
dmitriy365377 <tigr365377@gmail.com>
2020-04-04 19:43:02 +0300

    variables and Data types

---
+++ bin/variables.dart
@@ -0,0 +1,34 @@
+void main() {
+  var myAge = 35;
+  print(myAge);
+
+  int yourAge = 42;
+  print(yourAge);
+
+  var pi = 3.14;
+  print(pi);
+
+  double c = 2032323;
+  print(c);
+
+  dynamic numberOfKittens;
+  numberOfKittens = "There are no kittens!";
+  print(numberOfKittens);
+
+  numberOfKittens = "There are kittens!";
+  print(numberOfKittens); 
+
+  bool areThereNotKittens = false;
+  print(areThereNotKittens);
+
+  int numberOfPeople;
+  print(numberOfPeople);
+
+  var numbers = [1,2,3,4];
+  numbers.add(5);
+  print(numbers);
+
+  final morenumbers = const [7,8,9];
+
+}
+
