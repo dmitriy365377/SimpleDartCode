2a36b4ba53d8e7023f16bc9e78a76bfd782f385a
dmitriy365377 <tigr365377@gmail.com>
2020-04-04 21:13:03 +0300

    first task

---
+++ bin/variables/challangeForVariables.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Challange Time! Variables and Types...
+
+
+  //Create a variable to hold a number of apples that relies on type inference.
+  //Then create a variable oranges that is explicitly typed as int. Use the 
+  //greater than operator to compare the number of apples to the number of 
+  // oranges to produce a bool variable.
+
+  var apples = 8;
+  int oranges = 10;
+
+  bool compare = apples > oranges;
+
+  print(compare);
+  
+
+  // Create a variable speed set to 100, then re-assign the value to 110.
+  //Create an immutable value acceleration and set it`s value to 9.8. 
+
+  var speed = 100;
+  speed = 110;
+
+  const immutable = 9.8;
+ 
+}
