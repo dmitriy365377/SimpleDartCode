fc91452cc133f14737787e89f65eab56a673c5ca
dmitriy365377 <tigr365377@gmail.com>
2020-04-05 00:27:15 +0300

    string

---
+++ bin/string/challangeForString.dart
@@ -0,0 +1,12 @@
+void main() {
+  var directHit = '\u{1f3af}';
+  print(directHit); 
+
+  int age;
+  double height;
+  String err;
+
+  print(age);
+  print(height);
+  print(err);
+} 
---
+++ bin/string/string.dart
@@ -0,0 +1,11 @@
+void main () {
+  var firstName = "Dmitriy";
+  String lastName = "Verkhovtsov";
+
+  const String myName = "Dima";
+  var apples = 3;
+  var oranges = 4;
+  var totalFruit = "Total amount of fruit is ${apples + oranges} pieces";
+  print(totalFruit.length);
+
+}
