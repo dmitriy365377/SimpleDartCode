ba6e3b4ea8839f818c768f1bb6b7e7d34c9223d8
dmitriy365377 <tigr365377@gmail.com>
2020-04-08 19:33:58 +0300

    phone

--- bin/algo/algo.dart
+++ bin/algo/algo.dart
@@ -7,14 +7,23 @@ int multiply(int a, int b) {
 }
 
 int positiveSum(List<int> arr) {
-   List<int> positiveNumber = arr.where((num) => num >= 0).toList();
-  if (positiveNumber.length == 0 ) {
+  List<int> positiveNumber = arr.where((num) => num >= 0).toList();
+  if (positiveNumber.length == 0) {
     return 0;
-  } else { 
+  } else {
     return positiveNumber.reduce((value, element) => value + element);
   }
 }
 
+String createPhoneNumber(List numbers) {
+  List<String> listString = numbers.map((el) => el.toString()).toList();
+  var b = '(${listString[0]}${listString[1]}${listString[2]}) ${listString[3]}${listString[4]}${listString[5]}-${listString[6]}${listString[7]}${listString[8]}${listString[9]}';
+  print(b);
+  return b;
+}
+
+ 
+
 void main() {
-  print(positiveSum([]));
+ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 }
