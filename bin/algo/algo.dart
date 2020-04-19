b5866750fa13df6a9d18f1fe065c3be33b3cfd33
dmitriy365377 <tigr365377@gmail.com>
2020-04-19 09:13:01 +0300

    vowel count

--- bin/algo/algo.dart
+++ bin/algo/algo.dart
@@ -1,29 +1,45 @@
-num opposite(num n) {
-  print(n * -1);
-}
 
-int multiply(int a, int b) {
-  return a * b;
-}
 
-int positiveSum(List<int> arr) {
-  List<int> positiveNumber = arr.where((num) => num >= 0).toList();
-  if (positiveNumber.length == 0) {
-    return 0;
-  } else {
-    return positiveNumber.reduce((value, element) => value + element);
-  }
-}
+// num opposite(num n) {
+//   print(n * -1);
+// }
 
-String createPhoneNumber(List numbers) {
-  List<String> listString = numbers.map((el) => el.toString()).toList();
-  var b = '(${listString[0]}${listString[1]}${listString[2]}) ${listString[3]}${listString[4]}${listString[5]}-${listString[6]}${listString[7]}${listString[8]}${listString[9]}';
-  print(b);
-  return b;
-}
+// int multiply(int a, int b) {
+//   return a * b;
+// }
 
- 
+// int positiveSum(List<int> arr) {
+//   List<int> positiveNumber = arr.where((num) => num >= 0).toList();
+//   if (positiveNumber.length == 0) {
+//     return 0;
+//   } else {
+//     return positiveNumber.reduce((value, element) => value + element);
+//   }
+// }
+
+// String createPhoneNumber(List numbers) {
+//   List<String> listString = numbers.map((el) => el.toString()).toList();
+//   var b =
+//       '(${listString[0]}${listString[1]}${listString[2]}) ${listString[3]}${listString[4]}${listString[5]}-${listString[6]}${listString[7]}${listString[8]}${listString[9]}';
+//   print(b);
+//   return b;
+// }
+
+
+// import 'dart:svg';
+
+int getCount(String inputStr) {
+  dynamic count = 0;
+  final vowels = ['a', 'e', 'i', 'o', 'u'];
+  inputStr.runes.forEach((int rune) {
+    var character = new String.fromCharCode(rune);
+    if (vowels.contains(character)) {
+      count++;
+    }
+  }); 
+  return count;
+}
 
 void main() {
- createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+  getCount("abcde");
 }
